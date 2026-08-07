c3e69d4a660f527bee3350f190977a004bdb4cb5
Gabriel Hardin <planning4college67@gmail.com>
2026-08-06 23:57:35 -0500

    importing cleaned county column 8/6/26

--- sql_analysis_modelling/sql_capstone_analysis.sql
+++ sql_analysis_modelling/sql_capstone_analysis.sql
@@ -1,6 +1,6 @@
 SELECT *
 FROM fatalities
-/
+
 SELECT *
 FROM locations
 
@@ -17,5 +17,9 @@ SELECT begin_date_time,COUNT(deaths_direct) AS fatalities
 FROM storms INNER JOIN fatalities USING(event_id)
 ORDER BY fatalities DESC
 
-SELECT *
+SELECT cz_name, COUNT(event_narrative) as en
 FROM storms
+WHERE event_narrative LIKE '%road%'
+GROUP BY cz_name
+ORDER BY en DESC
+ 
